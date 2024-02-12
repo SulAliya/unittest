001848e9b91408c034b4f0618966bd3b9cb85078
Aliya Suleymanova <aliyasul2579@gmail.com>
2024-02-12 21:47:05 +0500

    test_dicts

---
+++ tests/test_dicts.py
@@ -0,0 +1,10 @@
+import unittest
+from utils import dicts
+
+class Testdicts(unittest.TestCase):
+
+    def test_get_val(self):
+        self.assertEqual(dicts.get_val({"vcs": "mercurial"}, "vcs", ), 'mercurial')
+        self.assertEqual(dicts.get_val({"vcs": "mercurial"}, "vcs", "git"), 'mercurial')
+        self.assertEqual(dicts.get_val({}, "vcs", "git"), 'git')
+        self.assertEqual(dicts.get_val({}, "vcs", "bazaar"), 'bazaar')
